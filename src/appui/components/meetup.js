bf213b930b7e0abbe0a11646a37c10fc335c6f57
webschuppen technik <webschuppentechnik@webschuppensMBP.fritz.box>
2018-05-29 11:42:59 +0200

    changes made in meetup

--- src/appui/components/meetup.js
+++ src/appui/components/meetup.js
@@ -1,6 +1,29 @@
 define('meetup/appui/components/meetup', [
-  'antie/widgets/component'
-], Component =>
+  'antie/widgets/component',
+  'antie/widgets/label',
+  'antie/widgets/container',
+  'antie/widgets/button'
+], (Component, Label, Container, Button) =>
   Component.extend({
-    // to be filled in meetup
+    init: function init() {
+      init.base.call(this);
+
+      this.onBeforerender = this.onBeforerender.bind(this);
+      this.addEventListener('beforerender', this.onBeforerender);
+    },
+
+    onBeforerender: function onBeforerender(data) {
+      this.appendChildWidget(
+        render(
+          <Container class="test">
+            <Label>Text</Label>
+            <Button
+              onClick={evt => {
+                console.log('test');
+              }}
+            />
+          </Container>
+        )
+      );
+    }
   }));
